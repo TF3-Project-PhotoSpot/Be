d93f3b7c8e83a11f66c1ce13aaebd736b5c837f9
code-tree <dltjdqls1200@naver.com>
2024-05-04 16:46:43 +0900

    fix: requestpart로 json 오는 경우 octet stream convert 추가
    - 예외 cause null 체크

--- src/main/java/com/tf4/photospot/global/exception/GlobalExceptionHandler.java
+++ src/main/java/com/tf4/photospot/global/exception/GlobalExceptionHandler.java
@@ -3,6 +3,7 @@ package com.tf4.photospot.global.exception;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatusCode;
@@ -131,8 +132,8 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 		@NotNull HttpStatusCode statusCode,
 		@NotNull WebRequest request
 	) {
-		final Throwable cause = ex.getCause();
-		log.error("{} : {}", cause.getClass(), cause.getMessage());
+		Optional.ofNullable(ex.getCause())
+			.ifPresent(cause -> log.error("{} : {}", cause.getClass(), cause.getMessage()));
 		log.error("{} : {}", ex.getClass(), ex.getMessage());
 		return createResponse(CommonErrorCode.UNEXPECTED_ERROR);
 	}
